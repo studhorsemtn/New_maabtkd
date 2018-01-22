76ce3bd35cd9a97ab9b36cfa4dd4cc5bb9f8a5ef
Bryon <studhorsemtn@gmail.com>
2018-01-21 19:47:32 -0700

    removed link to js file as no longer needed

--- TKDForms.php
+++ TKDForms.php
@@ -216,7 +216,7 @@ if (isset($_SESSION['pass'])) {
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="main.js"></script>
+    
 </body>
 
 </html>
