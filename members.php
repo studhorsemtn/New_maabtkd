5548e5217b8c4e61b31441b1afc0c93d23841d57
studhorsemtn <[studhorsemtn@gmail.com]>
2018-02-22 12:26:25 -0700

    updated titles and cleaned up code

--- members.php
+++ members.php
@@ -32,7 +32,7 @@ if(!isset($_SESSION['pass'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title></title>
+    <title>Judo and Taekwondo Training Videos</title>
     <link href="https://fonts.googleapis.com/css?family=Bree+Serif" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
